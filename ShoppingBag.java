74f9fca1223cf51d086a8ae02947114c5e119bb3
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 17:52:02 -0500

    Build ShoppingBag class [step 5]

--- ShoppingBag.java
+++ ShoppingBag.java
@@ -1,4 +1,13 @@
 import java.util.HashMap;
 import java.util.Map;
 
-// Create ShoppingBag class here
+public class ShoppingBag<T extends PricedItem<Integer>> {
+    // INSTANCE FIELDS
+    private Map<T, Integer> shoppingBag;
+
+    // CONSTRUCTOR
+    public ShoppingBag() {
+        this.shoppingBag = new HashMap();
+    }
+
+}
