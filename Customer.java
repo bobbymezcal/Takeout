5ae89d4b01e20fc46b232cedde0b539dd1aa5416
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 17:13:37 -0500

    Create Customer class [part 1]

--- Customer.java
+++ Customer.java
@@ -1,1 +1,23 @@
 // Create Customer class here
+public class Customer {
+    // INSTANCE FIELDS
+    private String name;
+    private int money;
+
+    // getters
+    public String getName() {
+        return this.name;
+    }
+    public int getMoney() {
+        return this.money;
+    }
+
+    // setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+}
