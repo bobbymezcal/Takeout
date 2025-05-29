0bb1d2ceed906709b8277f8797fe7793ab90442a
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 17:25:51 -0500

    Create Food class that implements PricedItem [step 3]

--- Food.java
+++ Food.java
@@ -1,1 +1,22 @@
-// Create Food class here
+public class Food implements PricedItem<Integer> {
+    // INSTANCE FIELDS
+    private String name, description;
+    private int price;
+
+    // CONSTRUCTOR
+    public Food(String name, String description, int price) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    @Override
+    public Integer getPrice() {
+        return price;
+    }
+
+    @Override
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+}
