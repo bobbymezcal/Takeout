2585ff2d6d7be503bb458b7ea81e63308079c15f
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 18:43:20 -0500

    Build TakeOutSimulator class [step 13]

--- TakeOutSimulator.java
+++ TakeOutSimulator.java
@@ -1,3 +1,15 @@
 import java.util.Scanner;
 
-// Create TakeOutSimulator class here
+public class TakeOutSimulator {
+    // INSTANCE FIELDS
+    private Customer customer;
+    private FoodMenu menu;
+    private Scanner input;
+
+    // CONSTRUCTOR
+    public TakeOutSimulator(Customer customer, FoodMenu menu, Scanner scanner) {
+        this.customer = customer;
+        this.menu = menu;
+        this.input = scanner;
+    }
+}
