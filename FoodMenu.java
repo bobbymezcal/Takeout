0dcbbd013369b55c244bd07db25d315380621386
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 18:20:23 -0500

    Add FoodMenu class with test constructor [step 8]

--- FoodMenu.java
+++ FoodMenu.java
@@ -1,4 +1,15 @@
 import java.util.ArrayList;
 import java.util.List;
 
-// Create FoodMenu class here
+public class FoodMenu {
+    // INSTANCE FIELDS
+    private List<Food> menu;
+
+    // CONSTRUCTOR
+    public FoodMenu() {
+        this.menu = new ArrayList<Food>;
+        menu.add(new Food("Tacos", "Yummy steak tacos", 15));
+        menu.add(new Food("Dumplings", "Delicious steamed dumplings", 10));
+        menu.add(new Food("Ramen", "Hot pork ramen", 12));
+    }
+}
