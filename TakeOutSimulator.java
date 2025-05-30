d1fbdebedc1e4540833618f4d90386bf24996d54
Bobby Baker <45774915+bobbymezcal@users.noreply.github.com>
2025-05-29 19:32:51 -0500

    Add getResponse method to TakeOutSimulator class [step 14]

--- TakeOutSimulator.java
+++ TakeOutSimulator.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 
 public class TakeOutSimulator {
     // INSTANCE FIELDS
@@ -11,5 +12,21 @@ public class TakeOutSimulator {
         this.customer = customer;
         this.menu = menu;
         this.input = scanner;
+    }
+
+    private <T> T getResponse(String userInputPrompt, UserInputRetriever<T> userInputRetriever) {
+        input = new Scanner(System.in);
+        while (true) {
+            try {
+                System.out.println(userInputPrompt);
+                int userInput = input.nextInt();
+                input.nextLine();
+                return userInputRetriever.produceOutput(userInput);
+            } catch (IllegalArgumentException e) {
+                System.out.println("That was not a valid input. Try again!");
+            } catch (InputMismatchException e) {
+                System.out.println("Input needs to be an 'int' type.");
+            }
+        }
     }
 }
